f7a3ce37d5d3f0e7295a29764e8c265f2d81aa90
Андрей <an-shafra2013@ya.ru>
2024-09-18 15:20:15 +0800

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    if second > 0:
+        div = first / second
+        return div
+    else:
+        return 'Ошибка'
+
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+def divide(first, second):
+    if second > 0:
+       div = first/second
+       return div
+    else:
+        return inf
+
